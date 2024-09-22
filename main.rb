fb1a700a2949b17f1db081bedfa8318c3de8dcbd
nkumar041 <nikumar041@gmail.com>
2024-09-22 17:35:08 -0400

    created the Card class

--- main.rb
+++ main.rb
@@ -1,5 +1,16 @@
-require_relative 'lib/game'
+# require_relative 'lib/game'
 
-game = Game.new
-game.add_players
-game.start
+# game = Game.new
+# game.add_players
+# game.start
+
+class Card
+    attr_reader :color, :shape, :shading, :number
+  
+    def initialize(color, shape, shading, number)
+      @color = color
+      @shape = shape
+      @shading = shading
+      @number = number
+    end
+end
