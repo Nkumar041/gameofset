f2de68375346c0f0138dcbdf397365886fbfa5b8
nkumar041 <nikumar041@gmail.com>
2024-09-22 19:33:03 -0400

    modified display of each card

--- Card.rb
+++ Card.rb
@@ -1,4 +1,5 @@
-# Create a card class
+require 'colorize'
+
 class Card
     attr_reader :number, :color, :shape, :shading
   
@@ -12,6 +13,24 @@ class Card
   
     # converts all card values into a string
     def to_s
-        "#{number} #{color} #{shape} #{shading}"
+        colorized_color = case @color
+                          when 'red' then 'red'.colorize(:red)
+                          when 'green' then 'green'.colorize(:green)
+                          when 'purple' then 'purple'.colorize(:magenta)
+                          end
+    
+        shape_symbol = case @shape
+                       when 'oval' then '●'
+                       when 'squiggle' then '~'
+                       when 'diamond' then '♦'
+                       end
+    
+        symbol_and_shading = case @shading
+                        when 'solid' then shape_symbol * @number + ' (solid)'
+                        when 'striped' then shape_symbol * @number + ' (striped)'
+                        when 'open' then shape_symbol * @number + ' (open)'
+                        end
+    
+        "#{colorized_color} #{shading_style}"
     end
 end
